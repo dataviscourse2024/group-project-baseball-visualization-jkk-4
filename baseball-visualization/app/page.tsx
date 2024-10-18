5f2d236df28cccd9e9a2567dac17eb0bc5d1daf8
lee1jaden_MSIModern <jadenlee1@att.net>
2024-10-18 13:43:13 -0600

    Deleted Next.js auto web features.

--- baseball-visualization/app/page.tsx
+++ baseball-visualization/app/page.tsx
@@ -1,95 +1,65 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <h3>CS-5630 / CS-6630 Homework 3</h3>
+      <address>
+        <span>Name: Jaden Lee  -</span>
+        <span>-  Email: u1417827@utah.edu   -</span>
+        <span>-  UID: u1417827</span>
+      </address>
+      <br />
+      <hr />
+      <br />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.toolbox}>
+
+        <span>
+          <label>Dataset:</label>
+          <select id="dataset">
+            <option value="covid_us">The US</option>
+            <option selected value="covid_utah">Utah</option>
+            <option value="covid_ca">California</option>
+            <option value="covid_ny">New York</option>
+          </select>
+        </span>
+        <span>
+          <label>Data Metric</label>
+          <select id="metric">
+            <option value="deaths">Deaths</option>
+            <option value="cases">Cases</option>
+          </select>
+        </span>
+
+        <span> <input type="checkbox" id="random" /> Random Subset </span>
+      </div>
+      <br />
+
+      <div className={styles.row + " " + styles.chart}>
+        <div className={styles.chart}>
+          <h2>Bar Charts</h2>
+          <div id="Barchart-div">
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className={styles.chart}>
+          <h2>Line Charts</h2>
+          <div id="Linechart-div"></div>
+        </div>
+      </div>
+
+      <div className={styles.row + " " + styles.chart}>
+        <div className={styles.chart}>
+          <h2>Area Charts</h2>
+          <div id="Areachart-div"></div>
+        </div>
+
+        <div className={styles.chart}>
+          <h2>Scatterplot</h2>
+          <div id="Scatterplot-div"></div>
+        </div>
+      </div>
     </div>
   );
 }

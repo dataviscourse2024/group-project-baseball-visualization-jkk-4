76fa979c31e830b3f974d372dc3a8b6f83a035a1
kaceyabbott11 <kacey.abbott@utah.edu>
2024-12-05 19:25:13 -0700

    Update
    Update

--- MLB Visualization/server.py
+++ MLB Visualization/server.py
@@ -7,7 +7,9 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 origins = [
-    "*"
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "https://dataviscourse2024.github.io"
 ]
 
 app.add_middleware(
@@ -23,25 +25,25 @@ async def root():
     return {"message": "Welcome to our baseball API. Open the '/docs' page to see the API.",
             "link": "http://localhost:8000/docs"}
 
-app.mount("/MLB Visualization/datasets", StaticFiles(directory="datasets"), name="datasets")
+app.mount("/datasets", StaticFiles(directory="datasets"), name="datasets")
 
 @app.get("/players")
 async def players():
-    personalInfo = pd.read_csv("./MLB Visualization/datasets/csv/People.csv",  encoding='latin-1')
+    personalInfo = pd.read_csv("./datasets/csv/People.csv",  encoding='latin-1')
     personalInfo = personalInfo.loc[personalInfo["birthYear"] > 1800]
     personalInfo = personalInfo[["playerID", "nameFirst", "nameLast"]]
     return personalInfo.to_dict(orient='records')
 
 @app.get("/batters/{player_id}")
 async def batters(player_id: str):
-    data = pd.read_csv("./MLB Visualization/datasets/csv/Batting.csv")
+    data = pd.read_csv("./datasets/csv/Batting.csv")
     data = data.loc[data["playerID"] == player_id]
     data = data[["yearID","TeamName","G","AB","R","H","2B","3B","HR","RBI","SB","CS","BB","SO","IBB","HBP","SH","SF","GIDP"]].fillna(0)
     return data.to_dict(orient='records')
 
 @app.get("/pitchers/{player_id}")
 async def pitchers(player_id: str):
-    data = pd.read_csv("./MLB Visualization/datasets/csv/Pitching.csv")
+    data = pd.read_csv("./datasets/csv/Pitching.csv")
     data = data.loc[data["playerID"] == player_id]
     data = data[["yearID", "TeamName", "W","L","G","GS","CG","SHO","SV","IPouts","H","ER","HR","BB","SO","BAOpp","ERA","IBB","WP","HBP","BK","BFP","GF","R","SH","SF","GIDP"]].fillna(0)
     return data.to_dict(orient='records')
